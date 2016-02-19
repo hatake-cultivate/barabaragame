40dfcc65361eecdfca66f29f6119c3af829e9b9f
Take Haru <sj0030rx@gmail.com>
2016-02-20 00:45:22 +0900

    first commit

--- RankingViewController.swift
+++ RankingViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class RankingViewController: UIViewController {
-
+    
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    
+    let defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        rankingLabel1.text = String(defaults.integerForKey("score1"))
+        rankingLabel2.text = String(defaults.integerForKey("score2"))
+        rankingLabel3.text = String(defaults.integerForKey("score3"))
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTop(){
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
